b8e608e0ce32918063b2f434efc5e97d97d4263e
RemcoSchrijver <remco.schrijver@hotmail.com>
2023-12-01 00:44:01 +0100

    Setup nix enviroment for scala and answers for day 1 2022 aoc

---
+++ 2022/flake.nix
@@ -0,0 +1,21 @@
+{
+    description = "Flake for AoC 2022 done in Scala";
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    inputs.flake-utils.url = "github:numtide/flake-utils";
+
+    outputs = { self, nixpkgs, flake-utils }:
+        flake-utils.lib.eachDefaultSystem (system:
+            let
+                pkgs = nixpkgs.legacyPackages.${system};
+            in
+            {
+                devShells.default = pkgs.mkShell {
+                    packages = [
+                        pkgs.scala-cli
+                        pkgs.metals
+                        pkgs.coursier
+                    ];
+                };
+            }
+        );
+}
